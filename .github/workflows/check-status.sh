2b536458c37d7fce5b3bf7ef51d0186404c89223
Jorge Maroto <patoroco@gmail.com>
2020-12-10 21:07:38 +0100

    Adding help to the check-status.sh

--- .github/workflows/check-status.sh
+++ .github/workflows/check-status.sh
@@ -5,5 +5,8 @@ else
   echo "The following files appear as changed:"
   git status --porcelain
   git diff
+
+  echo "Remember to run 'npm install && npm run pack' and commit these changes after doing changes
+  in the dependencies."
   false
 fi
